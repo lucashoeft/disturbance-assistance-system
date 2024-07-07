89b5b207188a426cccdd419eee5ae1f5db9ba9f2
Lucas Hoeft <26332559+lucashoeft@users.noreply.github.com>
2024-07-07 19:03:39 +0200

    Integrate Langchain

--- chainlit/demo.py
+++ chainlit/demo.py
@@ -2,129 +2,198 @@ import chainlit as cl
 from chainlit.types import ThreadDict
 import chainlit.data as cl_data
 import json
+import os
+from langchain_community.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+from chainlit.input_widget import Select, Switch, Slider
+from langfuse.callback import CallbackHandler
+import os
+import dotenv
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_postgres import PGVector
+from langchain_postgres.vectorstores import PGVector
+from langchain_postgres.chat_message_histories import PostgresChatMessageHistory
+import uuid
+import psycopg
+import random
+from langfuse.callback import CallbackHandler
+from langfuse import Langfuse
+
+dotenv.load_dotenv()
+
+OPEN_API_KEY = os.getenv('OPEN_AI_API_KEY')
+LANGFUSE_PUBLIC_KEY = os.getenv('LANGFUSE_PUBLIC_KEY')
+LANGFUSE_SECRET_KEY = os.getenv('LANGFUSE_SECRET_KEY')
+
+rnd = random.Random()
+rnd.seed("chat_session_1") # NOTE: Of course don't use a static seed in production
+random_uuid = uuid.UUID(int=rnd.getrandbits(128), version=4)
+session_id = str(random_uuid)
+
+langfuse_handler = CallbackHandler(
+    public_key=LANGFUSE_PUBLIC_KEY,
+    secret_key=LANGFUSE_SECRET_KEY,
+    host="http://host.docker.internal:3000",
+    session_id='chat_session_1',
+    user_id='user1'
+)
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+connection = "postgresql+psycopg://admin:admin@postgres:5432/vectordb"  # Uses psycopg3!
+
+vectorstore = PGVector(
+    embeddings=OpenAIEmbeddings(),
+    collection_name="my_docs",
+    connection=connection,
+    use_jsonb=True,
+)
+
+retriever = vectorstore.as_retriever()
+
+### Contextualize question ###
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. If multiple context information seem relevant list all of them"
+    "\n\n"
+    "{context}"
+)
+
+# Use three sentences maximum and keep the "answer concise.
+
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+conn_info = "postgresql://admin:admin@postgres:5432/chat_history"
+sync_connection = psycopg.connect(conn_info)
+table_name ="chat_history"
+
+history = PostgresChatMessageHistory(
+    table_name,
+    session_id,
+    sync_connection=sync_connection,
+)
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    lambda session_id: history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+config = {"configurable": {"session_id": session_id,}, "callbacks":[langfuse_handler]}
+    
+@cl.on_message
+async def main(message: cl.Message):
+
+    model = ChatOpenAI(streaming=True)
+    
+    config = {"configurable": {"session_id": 1,}, "callbacks":[langfuse_handler]}
+    
+    response = conversational_rag_chain.invoke(
+        {"input": message.content},
+        config=config
+    )
+
+    await cl.Message(response['answer']).send()
+
+# dotenv.load_dotenv()
+# OPEN_API_KEY = os.getenv('OPEN_AI_API_KEY')
+# LANGFUSE_PUBLIC_KEY = os.getenv('LANGFUSE_PUBLIC_KEY')
+# LANGFUSE_SECRET_KEY = os.getenv('LANGFUSE_SECRET_KEY')
 
 class CustomDataLayer(cl_data.BaseDataLayer):
   async def upsert_feedback(self, feedback: cl_data.Feedback) -> str:
-        filename = "results.json"
-        with open(filename,"r") as file:
-            try:
-                existing_data = json.load(file)
-            except json.JSONDecodeError:
-                existing_data = []
-        for data in existing_data:
-            # print(data)
-            try:
-                if data['id']==feedback.forId:
-                    data['feedback']=feedback.comment
-                    if feedback.value==0:
-                        data['positiveORnegative']="Negative"
-                    else:
-                        data['positiveORnegative']="Positive"
-            except:
-                pass
-        with open(filename, "w") as file:
-            json.dump(existing_data, file, indent=4)   
+        print(self, feedback) 
 
-from chainlit.input_widget import Select, Switch, Slider
+        langfuse = Langfuse(
+            public_key=LANGFUSE_PUBLIC_KEY,
+            secret_key=LANGFUSE_SECRET_KEY,
+            host="http://host.docker.internal:3000",
+        )
+        
+        trace = langfuse.trace(name = "llm-feature")
 
-
-@cl.on_chat_start
-async def start():
-    settings = await cl.ChatSettings(
-        [
-            Select(
-                id="Model",
-                label="OpenAI - Model",
-                values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
-                initial_index=0,
-            ),
-            Switch(id="Streaming", label="OpenAI - Stream Tokens", initial=True),
-            Slider(
-                id="Temperature",
-                label="OpenAI - Temperature",
-                initial=1,
-                min=0,
-                max=2,
-                step=0.1,
-            ),
-            Slider(
-                id="SAI_Steps",
-                label="Stability AI - Steps",
-                initial=30,
-                min=10,
-                max=150,
-                step=1,
-                description="Amount of inference steps performed on image generation.",
-            ),
-            Slider(
-                id="SAI_Cfg_Scale",
-                label="Stability AI - Cfg_Scale",
-                initial=7,
-                min=1,
-                max=35,
-                step=0.1,
-                description="Influences how strongly your generation is guided to match your prompt.",
-            ),
-            Slider(
-                id="SAI_Width",
-                label="Stability AI - Image Width",
-                initial=512,
-                min=256,
-                max=2048,
-                step=64,
-                tooltip="Measured in pixels",
-            ),
-            Slider(
-                id="SAI_Height",
-                label="Stability AI - Image Height",
-                initial=512,
-                min=256,
-                max=2048,
-                step=64,
-                tooltip="Measured in pixels",
-            ),
-        ]
-    ).send()
-
-
+        trace.score(
+            name="user-explicit-feedback",
+            value=feedback.value,
+            comment=feedback.comment,
+        )
+        
 @cl.on_settings_update
 async def setup_agent(settings):
     print("on_settings_update", settings)
-
-@cl.set_chat_profiles
-async def chat_profile():
-    return [
-        cl.ChatProfile(
-            name="GPT-3.5",
-            markdown_description="The underlying LLM model is **GPT-3.5**.",
-            icon="https://picsum.photos/200",
-        ),
-        cl.ChatProfile(
-            name="GPT-4",
-            markdown_description="The underlying LLM model is **GPT-4**.",
-            icon="https://picsum.photos/250",
-        ),
-    ]
 
 @cl.on_chat_start
 async def on_chat_start():
     chat_profile = cl.user_session.get("chat_profile")
     await cl.Message(
-        content=f"starting chat using the {chat_profile} chat profile"
+        content=f"Hey Buddy, chat using the {chat_profile} chat profile"
     ).send()
+
+    model = ChatOpenAI(streaming=True)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+
+    
+
+    cl.user_session.set("runnable", runnable)
+
 
 cl_data._data_layer=CustomDataLayer()
 
 @cl.step(type="message") 
 async def tool(name: str):
-    # Fake tool
     tool()
     await cl.sleep(1)
     return "Hello" + name
-
-@cl.on_chat_start
-def on_chat_start():
-    print("A new chat session has started!")
 
 @cl.on_stop
 def on_stop():
@@ -139,20 +208,5 @@ def on_chat_end():
 async def on_chat_resume(thread: ThreadDict):
     print("The user resumed a previous chat session!")
 
-@cl.on_message  # this function will be called every time a user inputs a message in the UI
-async def main(message: cl.Message):
-    """
-    This function is called every time a user inputs a message in the UI.
-    It sends back an intermediate response from the tool, followed by the final answer.
 
-    Args:
-        message: The user's message.
-
-    Returns:
-        None.
-    """
-    print("hello darkness")
-
-    response = f"Hello, you just sent: {message.content}!"
-    await cl.Message(response).send()
 
