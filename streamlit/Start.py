070ddf261b631c00f97588ffe66a971bfafab3a8
Lucas Hoeft <26332559+lucashoeft@users.noreply.github.com>
2024-07-09 17:35:03 +0200

    Simplify Streamlit Chat
    Only chat, no other features

--- streamlit/Start.py
+++ streamlit/Start.py
@@ -1,18 +1,143 @@
+import os
+import dotenv
 import streamlit as st
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_postgres import PGVector
+from langchain_postgres.vectorstores import PGVector
+from langchain_postgres.chat_message_histories import PostgresChatMessageHistory
+import uuid
+import psycopg
+import random
+from langfuse.callback import CallbackHandler
+
+dotenv.load_dotenv()
+
+OPEN_API_KEY = os.getenv('OPEN_AI_API_KEY')
+LANGFUSE_PUBLIC_KEY = os.getenv('LANGFUSE_PUBLIC_KEY')
+LANGFUSE_SECRET_KEY = os.getenv('LANGFUSE_SECRET_KEY')
+
+if 'db_session_id' not in st.session_state:
+
+    rnd = random.Random()
+    # rnd.seed(st.session_state.db_session_id) # NOTE: Of course don't use a static seed in production
+    random_uuid = uuid.UUID(int=rnd.getrandbits(128), version=4)
+    session_id = str(random_uuid)
+    
+    st.session_state['db_session_id'] = session_id
+
+print(st.session_state.db_session_id)
+session_id = st.session_state.db_session_id
+
+langfuse_handler = CallbackHandler(
+    public_key=LANGFUSE_PUBLIC_KEY,
+    secret_key=LANGFUSE_SECRET_KEY,
+    host="http://host.docker.internal:3000",
+    session_id=session_id
+)
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+connection = "postgresql+psycopg://admin:admin@postgres:5432/vectordb"  # Uses psycopg3!
+
+vectorstore = PGVector(
+    embeddings=OpenAIEmbeddings(),
+    collection_name="my_docs",
+    connection=connection,
+    use_jsonb=True,
+)
+
+retriever = vectorstore.as_retriever()
 
 st.set_page_config(
     # initial_sidebar_state="collapsed",
-    layout="wide",
-    page_title="Start - Assistenzsystem",
-    page_icon="https://static.wikia.nocookie.net/minecraft_gamepedia/images/b/b7/Crafting_Table_JE4_BE3.png"
+    # layout="wide",
+    page_title="Assistenzsystem"
 )
 
-st.header('Industrielles Assistenzsystem',anchor=False)
+st.header("Assistenzsystem", anchor=False)
 
-st.caption("Zur Erkennung, Behebung und Dokumentation von Störungen.")
+### Contextualize question ###
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
 
-# st.write("Dies ist ein Assistenzsystem, welches dabei unterstützt, Störungen zu erkennen, zu beheben und zu dokumentieren.")
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
 
-st.page_link("pages/1-Chat.py", label="Neue Störung")
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
 
-# st.page_link("http://localhost:8501/Chat?sessionId=" + st.session_state.db_session_id, label="Neue Störung")
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. If multiple context information seem relevant list all of them"
+    "\n\n"
+    "{context}"
+)
+
+# Use three sentences maximum and keep the "answer concise.
+
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+conn_info = "postgresql://admin:admin@postgres:5432/chat_history"
+sync_connection = psycopg.connect(conn_info)
+table_name ="chat_history"
+
+history = PostgresChatMessageHistory(
+    table_name,
+    session_id,
+    sync_connection=sync_connection,
+)
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    lambda session_id: history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+if len(history.messages) == 0:
+    history.add_ai_message("Hallo Werker bzw. Werkerin, ich unterstützte Sie bei Ihrer Tätigkeit in der Produktion. \n Ich verfüge über Informationen darüber, wie man Störungen beheben kann. Wie kann ich Ihnen weiterhelfen?")
+
+for msg in history.messages:
+    st.chat_message(msg.type).write(msg.content)
+
+user_input = st.chat_input("Sende eine Nachricht an das Assistenzsystem")
+
+if user_input:
+    st.chat_message("human").write(user_input)
+    
+    config = {"configurable": {"session_id": session_id,}, "callbacks":[langfuse_handler]}
+    
+    response = conversational_rag_chain.invoke(
+        {"input": user_input},
+        config=config
+    )
+
+    st.chat_message("ai").write(response['answer'])
